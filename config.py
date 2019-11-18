6bd4cdb569f581aa720ba0f1ab37168ad6bd5110
RoderickGu <jkgu@ucdavis.edu>
2019-11-18 01:20:01 -0800

    new_version

--- config.py
+++ config.py
@@ -1,28 +1,26 @@
 class Config():
 # if value is None, the value will be the default value in args.  
 
-    train_data_file = None
-    output_dir = None
-    eval_data_file = None
-    model_type = "gpt2"
-    model_name_or_path = 'gpt2' # if gpu memory enough, choose a bigger one
-    #mlm = None
-    #mlm_probability = None
-    config_name = None # optional when model_name_or_path exist
+    train_data_file = "train.jsonl"
+    output_dir = "output"
+    eval_data_file = "eval.jsonl"
+    # model_type = "gpt2"
+    # model_name_or_path = 'gpt2' # 'gpt2-medium', 'gpt2-large' if gpu memory enough, choose a bigger one
+    # config_name = None # optional when model_name_or_path exist
     tokenizer_name = None # optional when model_name_or_path exist
     cache_dir = None # Optional directory to store the pre-trained models downloaded from s3 (instread of the default one)
     block_size = None # Optional input sequence length after tokenization. "The training dataset will be truncated.
     
     # true or false
-    do_train = None
-    do_eval = None
-    evaluate_during_training = None
-    do_lower_case = None
+    do_train = True # True
+    do_eval = True # True
+    evaluate_during_training = True # True
+    do_lower_case = False# False
     
-    per_gpu_train_batch_size = None
-    per_gpu_eval_batch_size = None
-    gradient_accumulation_steps = None
-    learning_rate = None
+    per_gpu_train_batch_size = None # depends on total step
+    per_gpu_eval_batch_size = None # depends on gpu memory
+    gradient_accumulation_steps = None # depend on total step
+    learning_rate = None #TODO these hp, ask qingyang wu
     weight_decay = None
     adam_epsilon = None
     max_grad_norm = None
@@ -35,14 +33,14 @@ class Config():
 
     # true or false
     eval_all_checkpoints = None
-    no_cuda = None
+    no_cuda = False # False
     overwrite_output_dir = None
     overwrite_cache = None
 
-    seed = None
+    seed = 42
 
     # true or false
-    fp16 = None
+    fp16 = False # spped up but lose precision
 
     fp16_opt_level = None
     local_rank = None
--- test.py
+++ test.py
@@ -2,3 +2,21 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from torchfly.transformers import UnifiedTokenizer
 from torchfly.transformers import GPT2SimpleLM
 
+import jsonlines
+with open("examples.jsonl") as reader:
+    example = []
+    for obj in reader:
+        example.append(obj)
+
+assert len(example) == 1
+train_set = [example[0] for i in range(1000)]
+eval_set = [example[0] for i in range(1000)]
+
+with jsonlines.open('train.jsonl', mode='w') as writer:
+    for obj in train_set:
+        writer.write(obj)
+        
+
+with jsonlines.open('eval.jsonl', mode='w') as writer:
+    for obj in eval_set:
+        writer.write(obj)
