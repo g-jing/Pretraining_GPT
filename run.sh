7fb2500f3206bc1ebddd8e741e97e7686d8462b1
chongruo <chongruo@gmail.com>
2019-11-26 08:08:51 +0000

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nproc_per_node=2 main.py --fp16 --model_size="medium" --loss_type="all" --batch_size=7 2>&1 | tee log.txt
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 main.py --fp16 --model_size="medium" --loss_type="all" --batch_size=7 2>&1 | tee log.txt
