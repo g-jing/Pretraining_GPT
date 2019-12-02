6d221a240dfa6ae4aa6bd1850fe10d9f61054200
chongruo <chongruo@gmail.com>
2019-12-02 13:45:52 +0000

    new exp

--- run_2.sh
+++ run_2.sh
@@ -4,6 +4,6 @@ export OMP_NUM_THREADS=2
 python -m torch.distributed.launch\
     --nproc_per_node=8 main.py\
     --fp16 --model_size="small" \
-    --loss_type="all" --batch_size=48 \
+    --loss_type="all" --batch_size=40 \
     --kl_model_size="large" \
     --learning_rate=1e-5 2>&1 | tee log.txt
